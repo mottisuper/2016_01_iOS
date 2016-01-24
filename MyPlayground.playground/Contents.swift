b10adf22ba1c2fd9c3b53bd3bf99cadf5ac85001
Elad Lavi <eladlavi1@gmail.com>
2016-01-24 17:03:22 +0200

    variables and constants

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+var x = 8;
+x = 10;
+//x = "hello";
+var y:Int;
+y = 20;
+
+let s1:String = "Hello";
+//s1 = "hi";
+
+//String, Int, Float, Double, Bool
+
+//0.00000001230000000
+//1230.0
+
+let i:Int = -25;
+let f:Float = 3.14;
+let d:Double = 99.99;
+let b:Bool = true;
